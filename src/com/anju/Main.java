2152747bc75c9dc91c6916b6d477e2593346ed45
Anju Prema <anjuprema00@gmail.com>
2024-02-08 16:59:44 +0530

    first_commit

---
+++ src/com/anju/Main.java
@@ -0,0 +1,15 @@
+package com.anju;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a number:");
+		Scanner s = new Scanner(System.in);
+		int i = s.nextInt();
+		System.out.println("You entered :"+i);
+		System.out.println("exiting..");
+	}
+
+}
